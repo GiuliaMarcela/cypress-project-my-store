232e95749ca876b150837613637e1819c7692530
Giulia Marcela <giulia.arruda@ilabqa.com>
2022-08-11 13:23:59 -0300

    feat: :sparkles: adds custom command  to hide sensitive data
    - add custom command to hide sensitive data and document methods

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,17 +2,36 @@
  * @description Command used to validate if the url has the same pathname or search as expected.
  * @description Comando utilizado para validar se a url tem o mesmo pathname ou busca que o esperado.
  *
- * @param {string} query - Enter pathname or query to be validated.
+ * @param {string} text - Enter pathname or query to be validated.
  * Based on baseUrl in cypress configuration file.
- * @param {object} options - Type of validation.
+ * @param {object} options - Type of validation. Options { pathname: false, search: false }.
+ * @example cy.validate('?query=true', { search: true })
  * */
-Cypress.Commands.add(
-  'validate',
-  (query, options = { pathname: false, search: false }) => {
-    if (!options.pathname) {
-      cy.location().should((current) => expect(current.search).to.eq(query))
-    } else {
-      cy.location().should((current) => expect(current.pathname).to.eq(query))
-    }
+Cypress.Commands.add('validate', (text, options) => {
+  if (options && options.pathname) {
+    return cy
+      .location()
+      .should((current) => expect(current.pathname).to.eq(text))
   }
-)
+
+  return cy.location().should((current) => expect(current.search).to.eq(text))
+})
+
+/**
+ * @description Rewrite the type command to add functionality to hide sensitive data.
+ * @description Reescrita do comando type para adicionar funcionalidade de esconder dados sensíveis.
+ * @example cy.type('senhasupersecreta', { sensitive : true })
+ * */
+Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
+  if (options && options.sensitive) {
+    options.log = false
+
+    Cypress.log({
+      $el: element,
+      name: 'type',
+      message: '*'.repeat(text.length)
+    })
+  }
+
+  return originalFn(element, text, options)
+})
