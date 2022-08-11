d3868ddade2df521baa6eaa71d14b986552d67e1
Giulia Marcela <giulia.arruda@ilabqa.com>
2022-08-11 16:03:39 -0300

    feat: :heavy_plus_sign: adds cypress-mochawesome-reporter and configure it

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,23 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: true,
+  videoUploadOnPasses: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: false,
+    reportPageTitle: 'automation-practice-report',
+    reportFilename: '[status]--[name]-report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    quiet: true,
+    timestamp: 'dd-mmm-yyyy-HH-MM-ss'
+  },
+  trashAssetsBeforeRuns: true,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents (on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
     baseUrl: 'http://automationpractice.com/'
   }
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,4 @@
+import 'cypress-mochawesome-reporter/register'
 import 'cypress-real-events/support'
 import 'cypress-xpath'
 
